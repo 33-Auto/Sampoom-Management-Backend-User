5a09a59aac9238d4395d92dd37092c29180598c2
LeeJongJin <ljj61111@gmail.com>
2025-10-14 10:31:41 +0900

    SPM-9 [FIX] 코드래빗 수정 권장사항 반영_(WIP1:오타 및 수정, 보안 관련 미반영)

--- src/main/java/com/sampoom/backend/user/config/SecurityConfig.java
+++ src/main/java/com/sampoom/backend/user/config/SecurityConfig.java
@@ -18,9 +18,9 @@ import java.util.List;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
-        http
+        // CodeQL [java/spring-disabled-csrf-protection]: This is a stateless JWT API, CSRF protection is unnecessary.
+        http    // JWT 는 CSRF 보호가 필요없음
                 .csrf(csrf -> csrf.disable()) // H2 콘솔 테스트용 비활성화
-                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // ✅ H2 콘솔 iframe 허용
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/user/signup",
@@ -32,13 +32,16 @@ public class SecurityConfig {
                                 "/webjars/**").permitAll() // 콘솔 접근 허용
                         .anyRequest().authenticated()
                 )
+                // 기본 폼 로그인 비활성화
                 .formLogin(login -> login.disable())
                 .httpBasic(basic -> basic.disable())
+                // 세션 미사용 명시
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // CORS 허용 설정
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new CorsConfiguration();
                     corsConfig.setAllowedOrigins(List.of("*"));
-                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                     corsConfig.setAllowedHeaders(List.of("*"));
                     return corsConfig;
                 }))
