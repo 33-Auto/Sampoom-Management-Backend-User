3a020f0a69f0b7834284602e85b6ff1721ecfea5
LeeJongJin <ljj61111@gmail.com>
2025-10-30 21:15:36 +0900

    [FIX] Kafka 적용_(WIP9: 개발용 CORS 제거)

--- src/main/java/com/sampoom/user/api/user/entity/User.java
+++ src/main/java/com/sampoom/user/api/user/entity/User.java
@@ -17,19 +17,7 @@ public class User extends BaseTimeEntity {
 
     @Column(nullable = false, length = 50)
     private String userName;    // 사용자 이름
-//
-//    @Column(length = 50)
-//    private String workspace;   // 직장 분류(대리점, 창고, 공장)
-//
-//    @Column(length = 50)
-//    private String branch;      // 지점명
-//
-//    @Column(length = 50)
-//    private String position;    // 사용자 직책
 
     // 더티 체킹: 회원정보 수정용 setter만 공개 (다른 필드는 숨김)
     public void setUserName(String userName) { this.userName = userName; }
-//    public void setPosition(String position) { this.position = position; }
-//    public void setWorkspace(String workspace) { this.workspace = workspace; }
-//    public void setBranch(String branch) { this.branch = branch; }
 }
