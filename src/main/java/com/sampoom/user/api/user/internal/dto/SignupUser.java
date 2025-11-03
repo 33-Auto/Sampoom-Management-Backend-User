e94c8bbdcf183766868f2e58720a2bf8a0085f37
LeeJongJin <ljj61111@gmail.com>
2025-11-03 20:41:39 +0900

    [FEAT] 로그인 시 유저 조직 ID 포함 전체 프로필 조회 구현_(WIP3: Enum 타입 오류 해결)

--- src/main/java/com/sampoom/user/api/user/internal/dto/SignupUser.java
+++ src/main/java/com/sampoom/user/api/user/internal/dto/SignupUser.java
@@ -1,6 +1,7 @@
 package com.sampoom.user.api.user.internal.dto;
 
 import com.sampoom.user.common.entity.Position;
+import com.sampoom.user.common.entity.Workspace;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -13,7 +14,7 @@ import lombok.*;
 public class SignupUser {
     private Long userId;       // Auth에서 생성한 userId
     private String userName;   // 사용자 이름
-    private String workspace;  // 근무지(대리점, 창고, 공장 등)
+    private Workspace workspace;  // 근무지(대리점, 창고, 공장 등)
     private String branch;     // 지점명
     private Position position;   // 직책
 }
