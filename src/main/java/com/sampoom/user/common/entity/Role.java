2be1085e4e9d93d04829533dec3b04ab107f1142
LeeJongJin <ljj61111@gmail.com>
2025-11-10 17:12:29 +0900

    [FEAT] 관리 별 권한 조정_(NEW: USER->관리별 권한)

--- src/main/java/com/sampoom/user/common/entity/Role.java
+++ src/main/java/com/sampoom/user/common/entity/Role.java
@@ -1,7 +1,14 @@
 package com.sampoom.user.common.entity;
 
 public enum Role {
-    USER,    // 일반 직원
-    ADMIN    // 관리자
+    // 통합 관리자: 모든 일반 관리자의 관리에 접근 가능
+    ADMIN,
 
+    // 일반 관리자: 자신에 해당하는 관리만 접근 가능
+    MD,             // 기준 정보 관리
+    SALES,          // 판매 관리
+    INVENTORY,      // 재고 관리
+    PRODUCTION,     // 생산 관리
+    PURCHASE,       // 구매 관리
+    HR,             // 인사 관리
 }
