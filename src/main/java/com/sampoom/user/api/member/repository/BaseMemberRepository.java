9d72c6740a573f86fac2b592526c7cc18abfd360
LeeJongJin <ljj61111@gmail.com>
2025-11-11 01:49:16 +0900

    [FEAT] 관리 별 권한 조정_(WIP1: 진행중)

--- src/main/java/com/sampoom/user/api/member/repository/BaseMemberRepository.java
+++ src/main/java/com/sampoom/user/api/member/repository/BaseMemberRepository.java
@@ -1,8 +1,6 @@
 package com.sampoom.user.api.member.repository;
 
 import com.sampoom.user.common.entity.BaseMemberEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
