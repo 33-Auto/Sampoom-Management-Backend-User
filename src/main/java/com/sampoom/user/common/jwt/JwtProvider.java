52483a859a4aaaf30e4577bfcfdee467a38f1a0b
LeeJongJin <ljj61111@gmail.com>
2025-11-10 09:21:19 +0900

    [FIX] 토큰 발행 방 법 변경(NEW: 개인/공개키 방식(비대칭 키))

--- src/main/java/com/sampoom/user/common/jwt/JwtProvider.java
+++ src/main/java/com/sampoom/user/common/jwt/JwtProvider.java
@@ -13,19 +13,34 @@ import org.springframework.stereotype.Component;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @Component
 public class JwtProvider {
-    private final Key key;
+//    private final Key key;
+    private final PublicKey publicKey;
 
-    // Auth에서 들어오는 Key의 초기화 (캐싱용 객체)
-    public JwtProvider(@Value("${jwt.secret}") String secret) {
-        if (secret.getBytes(StandardCharsets.UTF_8).length < 32) {
-            throw new BadRequestException(ErrorStatus.TOO_SHORT_SECRET_KEY);
-        }
-        this.key = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
-                SignatureAlgorithm.HS256.getJcaName());
+    public JwtProvider(@Value("${jwt.public-key-base64}") String publicKeyBase64) throws Exception {
+        this.publicKey = loadPublicKey(publicKeyBase64);
     }
+
+    private PublicKey loadPublicKey(String base64) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(base64);
+        return KeyFactory.getInstance("RSA")
+                .generatePublic(new X509EncodedKeySpec(keyBytes));
+    }
+
+//    // Auth에서 들어오는 Key의 초기화 (캐싱용 객체)
+//    public JwtProvider(@Value("${jwt.secret}") String secret) {
+//        if (secret.getBytes(StandardCharsets.UTF_8).length < 32) {
+//            throw new BadRequestException(ErrorStatus.TOO_SHORT_SECRET_KEY);
+//        }
+//        this.key = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+//                SignatureAlgorithm.HS256.getJcaName());
+//    }
 
     public Claims parse(String token) {
         if (token == null) {
@@ -35,7 +50,7 @@ public class JwtProvider {
             throw new BadRequestException(ErrorStatus.BLANK_TOKEN);
         }
         try{
-            return Jwts.parserBuilder().setSigningKey(key).build()
+            return Jwts.parserBuilder().setSigningKey(publicKey).build()
                     .parseClaimsJws(token).getBody();
         }
         catch (ExpiredJwtException e) {
